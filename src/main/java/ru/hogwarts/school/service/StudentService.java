7e50285ec74eee57813b3019644549c49bf33b0d
ArtemGRB <artemgrb@gmail.com>
2025-05-12 17:02:53 +0700

    HW-4.4

--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.exception.StudentNotFoundException;
 import ru.hogwarts.school.model.Faculty;
@@ -12,6 +14,8 @@ import java.util.Set;
 @Service
 public class StudentService {
 
+    Logger logger = LoggerFactory.getLogger(StudentService.class);
+
     private StudentRepository studentRepository;
 
     public StudentService(StudentRepository studentRepository) {
@@ -19,6 +23,7 @@ public class StudentService {
     }
 
     public Student addStudent(Student student) {
+        logger.info("Was invoked method for add student");
         return studentRepository.save(student);
     }
 
@@ -28,13 +33,16 @@ public class StudentService {
             student = studentRepository.findById(id)
                     .orElseThrow(()->new StudentNotFoundException("Студент по данному ID не найден"));
         } catch (StudentNotFoundException e) {
+            logger.error("There is not student with id = " + id);
             throw new RuntimeException(e);
         }
+        logger.info("Was invoked method for get student by id = " + id);
         return student;
     }
 
     public Student updateStudent(long id, Student student) {
         student.setId(id);
+        logger.info("Was invoked method for update student");
         return studentRepository.save(student);
     }
 
@@ -42,33 +50,39 @@ public class StudentService {
         Student student = studentRepository.findById(id)
                 .orElseThrow(()->new StudentNotFoundException("Студент по данному ID не найден"));
         studentRepository.deleteById(id);
+        logger.info("Was invoked method for delete student");
             return student;
     }
 
     public Set<Student> filterStudent(int age) {
+        logger.info("Was invoked method for ");
         return studentRepository.findByAge(age);
     }
 
     public Set<Student> getStudentsByAgeBetween(int min, int max) {
+        logger.info("Was invoked method for filter student");
         return studentRepository.findByAgeBetween(min, max);
     }
 
     public Faculty getFacultyOfStudentById(long id) {
+        logger.info("Was invoked method for get faculty of student by Id = " + id);
         return studentRepository.findById(id)
                 .orElseThrow(() -> new StudentNotFoundException("Студент по данному ID не найден"))
                 .getFaculty();
     }
 
     public int getCountStudents(){
+        logger.info("Was invoked method for get count students");
         return studentRepository.getCountStudents();
     }
 
     public int getAvgAgeOfStudents(){
+        logger.info("Was invoked method for get avg ag of students");
         return studentRepository.getAvgAgeOfStudents();
     }
 
-
     public List<Student> getEndFiveStudents(){
+        logger.info("Was invoked method for get end five students");
         return studentRepository.getEndFiveStudents(getCountStudents()-5);
     }
 
